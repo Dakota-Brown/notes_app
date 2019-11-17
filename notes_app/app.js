4efcc28072cae7c1994c9391997128178790eb4a
Dakota <dakota.brown@onevisionresources.com>
2019-11-17 14:56:38 -0500

    Remove Note Function

--- notes_app/app.js
+++ notes_app/app.js
@@ -1,6 +1,6 @@
 const chalk = require('chalk')
 const yargs = require('yargs')
-const getNotes = require('./notes.js')
+const notes = require('./notes.js')
 
 
 //Customize Yargs Version
@@ -23,8 +23,7 @@ yargs.command({
         }
     },
     handler: function(argv) {
-        console.log('Title: ' + argv.title)
-        console.log('Body: ' + argv.body)
+        notes.addNote(argv.title, argv.body)
     }
 })
  
@@ -32,8 +31,14 @@ yargs.command({
 yargs.command({
     command: 'remove',
     describe: 'Remove a note',
-    handler: function() {
-        console.log('Removing the note')
+    builder: {
+        title: {
+            describe: 'Title of the note to be removed.',
+            demandOption: true,
+            type: 'string'
+    }},
+    handler: function(argv) {
+        notes.removeNote(argv.title)
     }
 })
 
